1196d90e9672bfc539d0843cbc1252ddcfafdf74
Touka-Agra <112978117+Touka-Agra@users.noreply.github.com>
2025-08-01 22:03:55 +0300

    Explore @SpringBootApplication

--- springboot_fundamentals/DemoApplication.java
+++ springboot_fundamentals/DemoApplication.java
@@ -2,8 +2,9 @@ package com.example.demo.springboot_fundamentals;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class }) //Task 3
 public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
